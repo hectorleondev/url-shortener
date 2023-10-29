f8f67167466192061f301caa456cce2c2e02754a
hectorleondev <payorayo@gmail.com>
2023-10-29 15:57:53 -0700

    Added new field in the table, created model and new db service file

---
+++ src/data/model/url.py
@@ -0,0 +1,42 @@
+import os
+from datetime import datetime
+
+from pynamodb.indexes import GlobalSecondaryIndex, AllProjection
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute, UTCDateTimeAttribute
+
+
+class UrlPathIndex(GlobalSecondaryIndex):
+    class Meta:
+        projection = AllProjection()
+
+    url_path = UnicodeAttribute(hash_key=True)
+
+
+class UrlModel(Model):
+    """
+    A model with an index
+    """
+
+    class Meta:
+        table_name = os.getenv("URL_TABLE")
+        region = os.getenv("REGION", "us-east-1")
+
+    url_id = UnicodeAttribute(hash_key=True)
+    url_path = UnicodeAttribute()
+    url_title = UnicodeAttribute()
+    created_at = UTCDateTimeAttribute(null=False, default=datetime.now())
+
+    url_path_index = UrlPathIndex()
+
+    def to_dict(self):
+        """
+        Retrieves the model as a dictionary
+        :return:
+        """
+        _dict_data = {
+            "id": self.url_id,
+            "path": self.url_path,
+            "title": self.url_title,
+        }
+        return _dict_data
---
+++ src/services/db.py
@@ -0,0 +1,42 @@
+from typing import Optional
+
+from src.data.model.url import UrlModel
+
+
+def save_url(url_id: str, url_path: str, url_title: str):
+    """
+    Save new url
+    :param url_id:
+    :param url_path:
+    :param url_title:
+    :return:
+    """
+    url_model = UrlModel()
+    url_model.url_id = url_id
+    url_model.url_path = url_path
+    url_model.url_title = url_title
+    url_model.save()
+
+
+def get_url(url_id: str) -> Optional[UrlModel]:
+    """
+    Get url
+    :param url_id:
+    :return:
+    """
+    try:
+        return UrlModel.get(hash_key=url_id)
+    except UrlModel.DoesNotExist:
+        return None
+
+
+def get_url_by_path(url_path: str) -> Optional[UrlModel]:
+    """
+    Get url
+    :param url_path:
+    :return:
+    """
+    try:
+        return UrlModel.url_path_index.get(hash_key=url_path)
+    except UrlModel.DoesNotExist:
+        return None
