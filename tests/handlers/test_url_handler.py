7c344b55561e96650bd802998f308c7731f9d5ce
hectorleondev <payorayo@gmail.com>
2023-10-30 07:55:41 -0700

    Added new unit test for new endpoint retrieve shortcode

--- tests/handlers/test_url_handler.py
+++ tests/handlers/test_url_handler.py
@@ -30,7 +30,7 @@ def create_table_mock():
 
 
 def add_new_record(item):
-    dynamodb = boto3.resource('dynamodb', "us-east-1")
+    dynamodb = boto3.resource("dynamodb", "us-east-1")
     table = dynamodb.Table("url_table")
     url_model = UrlModel()
     url_model.url_id = item["id"]
@@ -43,52 +43,47 @@ class TestUrlHandler:
     @mock_dynamodb2
     def test_new_create_shortcode_handler(self):
         from src.handlers import url_handler
+
         create_table_mock()
-        _event = {
-            "body": json.dumps({
-                "url": "https://www.xxxx.com/blog/test.html"
-            })
-        }
+        _event = {"body": json.dumps({"url": "https://www.xxxx.com/blog/test.html"})}
 
         response = url_handler.create_shortcode_handler(_event, None)
         assert response["statusCode"] == 201
         assert "shortcode" in json.loads(response["body"])
 
     @mock_dynamodb2
-    def test_existing_short_code_handler(self, mocker):
+    def test_existing_short_code_handler_error(self, mocker):
         from src.handlers import url_handler
-        expected = {
-            "shortcode": "xxxxx"
-        }
+
+        expected = {"message": "The url is already used for another short link"}
 
         create_table_mock()
-        add_new_record({
-            "id": "1111111",
-            "url": "https://www.xxxx.com/blog/test.html",
-            "title": "title link"
-        })
+        add_new_record(
+            {
+                "id": "1111111",
+                "url": "https://www.xxxx.com/blog/test.html",
+                "title": "title link",
+            }
+        )
 
         mocker.patch("src.controller.url_controller.to_base62", return_value="xxxxx")
 
         _event = {
-            "body": json.dumps({
-                "url": "https://www.xxxx.com/blog/test.html",
-                "title": "title link"
-            })
+            "body": json.dumps(
+                {"url": "https://www.xxxx.com/blog/test.html", "title": "title link"}
+            )
         }
         response = url_handler.create_shortcode_handler(_event, None)
-        assert response["statusCode"] == 201
-        body = json.loads(response["body"])
-        assert body["shortcode"] == expected["shortcode"]
+        assert response["statusCode"] == 400
+        assert json.loads(response["body"]) == expected
 
     def test_invalid_url_error(self, mocker):
         from src.handlers import url_handler
 
         _event = {
-            "body": json.dumps({
-                "url": "http/www.xxxx.com/blog/test.html",
-                "title": "title link"
-            })
+            "body": json.dumps(
+                {"url": "http/www.xxxx.com/blog/test.html", "title": "title link"}
+            )
         }
         response = url_handler.create_shortcode_handler(_event, None)
         assert response["statusCode"] == 400
@@ -97,22 +92,17 @@ class TestUrlHandler:
     def test_retrieve_url_data(self, mocker):
         from src.handlers import url_handler
 
-        expected = {
-            "url": "https://www.xxxx.com/blog/test.html",
-            "title": "title link"
-        }
+        expected = {"url": "https://www.xxxx.com/blog/test.html", "title": "title link"}
         create_table_mock()
-        add_new_record({
-            "id": 1111111,
-            "url": "https://www.xxxx.com/blog/test.html",
-            "title": "title link"
-        })
-
-        _event = {
-            "queryStringParameters": {
-                "shortcode": "xxxxx"
+        add_new_record(
+            {
+                "id": 1111111,
+                "url": "https://www.xxxx.com/blog/test.html",
+                "title": "title link",
             }
-        }
+        )
+
+        _event = {"queryStringParameters": {"shortcode": "xxxxx"}}
 
         mocker.patch("src.controller.url_controller.to_base10", return_value=1111111)
 
@@ -132,11 +122,7 @@ class TestUrlHandler:
             "message": "The shortcode is invalid",
         }
 
-        _event = {
-            "queryStringParameters": {
-                "shortcode": "AF643$"
-            }
-        }
+        _event = {"queryStringParameters": {"shortcode": "AF643$"}}
 
         response = url_handler.retrieve_url_data_handler(_event, None)
         assert response["statusCode"] == 400
@@ -145,9 +131,71 @@ class TestUrlHandler:
 
     def test_invalid_shortcode_validation_error(self, mocker):
         from src.handlers import url_handler
-        _event = {
-            "queryStringParameters": {}
-        }
+
+        _event = {"queryStringParameters": {}}
 
         response = url_handler.retrieve_url_data_handler(_event, None)
+        assert response["statusCode"] == 400
+
+    @mock_dynamodb2
+    def test_retrieve_existing_short_code_handler(self, mocker):
+        from src.handlers import url_handler
+
+        expected = {"shortcode": "xxxxx"}
+
+        create_table_mock()
+        add_new_record(
+            {
+                "id": "1111111",
+                "url": "https://www.xxxx.com/blog/test.html",
+                "title": "title link",
+            }
+        )
+
+        mocker.patch("src.controller.url_controller.to_base62", return_value="xxxxx")
+
+        _event = {
+            "queryStringParameters": {"url": "https://www.xxxx.com/blog/test.html"}
+        }
+        response = url_handler.retrieve_shortcode_handler(_event, None)
+        assert response["statusCode"] == 200
+        assert json.loads(response["body"]) == expected
+
+    @mock_dynamodb2
+    def test_retrieve_existing_short_code_not_found_url_error(self, mocker):
+        from src.handlers import url_handler
+
+        expected = {"message": "The url not found"}
+
+        create_table_mock()
+        add_new_record(
+            {
+                "id": "1111111",
+                "url": "https://www.xxxx.com/blog/test.html",
+                "title": "title link",
+            }
+        )
+
+        _event = {
+            "queryStringParameters": {"url": "https://www.xxxx.com/blog/test_two.html"}
+        }
+        response = url_handler.retrieve_shortcode_handler(_event, None)
+        assert response["statusCode"] == 400
+        assert json.loads(response["body"]) == expected
+
+    @mock_dynamodb2
+    def test_retrieve_existing_short_code_validation_error(self, mocker):
+        from src.handlers import url_handler
+
+        create_table_mock()
+        add_new_record(
+            {
+                "id": "1111111",
+                "url": "https://www.xxxx.com/blog/test.html",
+                "title": "title link",
+            }
+        )
+
+        _event = {"queryStringParameters": {}}
+        response = url_handler.retrieve_shortcode_handler(_event, None)
         assert response["statusCode"] == 400
